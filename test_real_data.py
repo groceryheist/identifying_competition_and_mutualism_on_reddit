67b9933ef0ec87945c3501962f070240db29e3fe
Nate E TeBlunthuis <nathante@mox2.hyak.local>
2020-07-20 23:12:54 -0700

    git-annex in Data for the VAR modeling project

--- test_real_data.py
+++ test_real_data.py
@@ -12,11 +12,14 @@ test_df = test_df.rename(mapper = lambda s: s.replace(".","_"),axis='columns')
 test_df = test_df.loc[test_df.subreddit.isin({"washington","seattle","eastside","seahawks"})]
 test_df = test_df.loc[test_df.week > datetime.fromisoformat("2012-01-01")]
 test_df_fit = test_df.loc[test_df.week < datetime.fromisoformat("2016-01-01")]
+
 test_df_forecast = test_df.loc[(test_df.week >= datetime.fromisoformat("2016-01-01")) & (test_df.week < datetime.fromisoformat("2016-07-01"))]
 ytab = test_df_fit.pivot(index='week',columns='subreddit',values='N_authors')
 ytab = ytab.reset_index(drop=False)
 
 y = ytab.drop('week',axis='columns').to_numpy()
+
+forecast_len = 30
 
 test_data = {'y':y.T,
              'N':y.shape[0],
@@ -26,13 +29,12 @@ test_data = {'y':y.T,
              'scale_diag':1,
              'scale_offdiag':0,
              'df':10,
-             'forecast_len':30,
+             'forecast_len':forecast_len,
              'es' : [1,0], # top-level prior for the means of the means (diag, off-diag)
              'fs' : [np.sqrt(1.4), np.sqrt(1.4)], # top-level prior for the precision of means. a pretty tight prior seems to help avoid multimodality
              'gs' : [2.1,2.1], # top-level prior for the position of the scales,
              'hs' : [1/3,1/3], # top-level prior for the precision of the scales,
              'df':10,
-             'forecast_len':25,
              'alpha':0,
              'sd0': 7, # hyper prior precision of mu0
              'g0':4, # hyper prior for variance of m0
@@ -45,8 +47,16 @@ test_data['n_seas_levels'] = [3]
 
 # gotta build it.
 
+yforecast = test_df_forecast.pivot(index='week',columns='subreddit',values='N_authors').reset_index(drop=False)
+
+# gotta build it.
+
 seahawks_season = load_seahawks_seasonality(ytab)
 test_data['season'] = seahawks_season.season_cat
+
+seahawks_forecast = load_seahawks_seasonality(yforecast)
+test_data['forecast_season'] = seahawks_forecast.season_cat
+test_data['forecast_len'] = len(seahawks_forecast)
 
 ## STILL NEED TO ADD SEASONALITY TO THE FORECAST
 
@@ -62,4 +72,5 @@ test_seasonality_fit  = unpickle_or_create("stan_models/test_seasonality_fit.pkl
                                            function=heaps_pois_seasonality.sampling,
                                            chains=4,
                                            data=test_data,
-                                           control={'adapt_delta':0.95,'max_treedepth':20})
+                                           iter=3000,
+                                           control={'adapt_delta':0.99,'max_treedepth':20})
