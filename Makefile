c1c1e19af2fabab31b367a0dbd4bf9e494298332
Nate E TeBlunthuis <nathante@mox2.hyak.local>
2020-07-21 14:01:21 -0700

    Create intermediate dataset with just the subreddits we'll use.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: data/seattle_subreddit_submissions.feather plots/subreddit_posts_timeseries data/seasonality_levels.feather
+all: data/seattle_subreddit_submissions.feather plots/subreddit_posts_timeseries data/seasonality_levels.feather data/included_timeseries.feather
 
 data/seattle_subreddit_submissions.feather: pull_seattle_subs.py comdata_output/reddit_submissions_by_subreddit.parquet
 	echo "Pulling subreddit histories from parquet"; \
@@ -9,9 +9,13 @@ data/seattle_subreddit_submissions.feather: pull_seattle_subs.py comdata_output/
 data/seasonality_levels.feather: seasonality.py
 	python3 seasonality.py
 
+data/included_timeseries.feather: choose_subreddits.R data/seattle_subreddit_submissions.feather
+	Rscript choose_subreddits.R
+
 plots/subreddit_posts_timeseries: data/seattle_subreddit_submissions.feather
 	Rscript describe_subreddit_activity.R
 
 clean_intermediate:
 	rm data/seattle_subreddit_submissions.feather
+	rm data/included_timeseries.feather
 	rm plots/subreddit_posts_timeseries/*
--- choose_subreddits.R
+++ choose_subreddits.R
@@ -1,4 +1,6 @@
-## This file contains logic for deciding which subreddits and time periods to include in the analysis
+## This is a script for 
+
+## It contains logic for deciding which subreddits and time periods to include in the analysis
 ## These settings were chosen by inspecting time series plots of the number of submissions and comments to each subreddit.
 ## We wish to select data where the assumptions of a gaussian or poisson VAR(p) model are plausible.
 ## These assumptions are: 1. no time-varying parameters, 2. no omitted variables, 3. no marginal over-dispersion (for poisson), marginal lognormality (for gaussian).
@@ -6,6 +8,7 @@
 
 ## based on plots of unique posters each week
 # not enough activity
+
 library(arrow)
 
 def_exclude = c('anacortes','auburnwa','bainbridgeisland','ballard','bellevuewa','belltown','bothell','bremerton','burien','camaswashington','capitolhillsea','clallam','destinationwa','edmonds','emeraldcityfoodies','federalway','fishingwashington','foodseattle','forkswa','grandcoulee','graysharbor','kentwa','kexp','kingcounty','kirkland','kittitas','lewiscounty','longview','lynnwood','lynwoodwa','marysville','mount_rainier','mukilteo','newsofseattle','northseattle','oceanshores','olympicnationalpark','olympic_peninsula','ortingwa','piercecountywa','pnwfestivals','politicswa','portangeles','portorchard','porttownsend','poulsbo','pugetlist','pugetsound','puyallupwa','rainier','renton_wa','sammamish','sanjuanislands','seatac','seattleapartments','seattle_bicycling','seattlecars','seattledragons','seattleevents','seattleents','seattlehistory','seattlehomelessfires','seattlehousing','seattlejobs','seattlemovienight','seattlemusic','seattlephotograhy','seattlequestions','seattleseawolvesrugby','seattlestorm','seattlethunderbirds','seattleu','seattlewarecipes','seawa','shoreline','skagit','skagitcounty','skagitvalley','snohomishcounty','snohomish','snoqualmie','southseattle','stevenspass','the_seattle','tukwila','tulalip','tumwater','uwt','veganseattle','wabeer','wallawalla','waoutdoors','washingtonforsanders','whidbey','wholesomeseattle','wizardsuniteseattle','woodinville','wsucougars','yelm')
@@ -13,7 +16,14 @@ def_exclude = c('anacortes','auburnwa','bainbridgeisland','ballard','bellevuewa'
 might_include = c('cwu','ewu','kitsap','redmond','renton','seawa','sonics','wenatchee','yakima')
 
 include=c('bellevue','bellingham','cascadia','circlejerkseattle','eastside','everett','hiphopheadsnorthwest','olympia','pacificnorthwest','pnws','pokemongoseattle','seajobs','sealist','seattlebike','seaents','seattle','seattlewa','spokane','tacoma','tricitieswa','vancouverwa','wa_guns','washington')
+
+# getting seasonality for the seattle reign is another hassle. But it would be cool to be inclusive.
 might_include_seasonality = c('reignfc')
+
+## huskies clearly has football season seasonality.
+## udub just looks like college season, but there might be some football mixed in
+## the same is probably true of wsu and wwu 
+## maybe quarterly seasonality will be good enough for pnwriders and pnwhiking. Let's go with that for now so I don't have to write any new code. 
 include_seasonality = c("huskies",'mariners','pnwhiking','pnwriders','seahawks','soundersfc','udub','wsu','wwu')
 #huskies has 
 potential_competitors_excluded = c('bellevuewa','seawa','seattlejobs','seattle_bicycling','wsucougars','seattleents')
@@ -22,6 +32,7 @@ source("helper.R")
 
 df <- load_weekly_posts()
 
-test_df <- df[subreddit %in% c("seattle","seattlewa","seahawks","washington","eastside","soundersfc")]
+df <- df[subreddit %in% c(include,include_seasonality)]
+write_feather(df,'data/included_timeseries.feather',compression="uncompressed")
 
-write_feather(test_df,'data/test_ts.feather',compression="uncompressed")
+
