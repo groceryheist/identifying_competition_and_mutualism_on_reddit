f7e848139cdf4cc3fbff4c3ad05508f8ee1c7897
Nate E TeBlunthuis <nathante@mox2.hyak.local>
2020-08-10 16:55:44 -0700

    Test model interpretation code using results from the full VAR1 model.

--- model_interpretation.R
+++ model_interpretation.R
@@ -2,6 +2,7 @@ library(arrow)
 library(Matrix)
 library(data.table)
 library(ggplot2)
+library
 
 load.draws <- function(model.name){
     draws <- read_feather(paste0(file.path("stan_models",model.name),'.feather'))
@@ -23,8 +24,8 @@ extract.params <- function(draws){
     phi.cols <- names(draws)[grepl('phi',names(draws))]
     phi.max <- phi.cols[length(phi.cols)]
     fields <- strsplit(phi.max,',')[[1]]
-    p <- as.integer(substr(fields[1],5,5))
-    m <- as.integer(substr(fields[2],1,1))
+    p <- as.integer(substr(fields[1],5,10000))
+    m <- as.integer(substr(fields[2],1,10000))
     phi <- apply(draws[,phi.cols,with=F],1,function(df) array(as.list(as.numeric(df)),dim=c(m,m,p)))
 
     sigma.cols <- names(draws)[grepl('Sigma',names(draws))]
@@ -121,7 +122,7 @@ irf.to.plotdata <- function(irf, matnames){
     stat.rows <- function(d, matnames){
 
         comp.stats <- function(draws){
-            q <- quantile(draws,probs=c(0.05,0.2,0.5,0.8,0.95))
+            q <- quantile(draws,probs=c(0.025,0.05,0.15,0.2,0.5,0.8,0.85,0.95,0.975))
             result <- list("mean" = mean(draws),
                            "var" = var(draws))
             result <- c(result,q)
@@ -147,7 +148,7 @@ irf.to.plotdata <- function(irf, matnames){
     }
 
     result <- list()
-    for(i in 1:length(irf)){
+ for(i in 1:length(irf)){
         rows <- stat.rows(irf[[i]], matnames)
         rows <- lapply(rows, function(r) c(r,list('x'=i)))
         result <- c(result,rows)
@@ -155,13 +156,13 @@ irf.to.plotdata <- function(irf, matnames){
     return(rbindlist(result))
 }
 
-plot.irf <- function(irf, matnames){
+plot.irf <- function(plotdata, matnames){
+    plot.data <- copy(plotdata)
     
-    plot.data <- irf.to.plotdata(irf, c('seattle','seahawks'))
     plot.data <- plot.data[,":="(name.row = factor(name.row,levels=matnames),
                                  name.col = factor(name.col,levels=matnames))]
-    setnames(plot.data,old=c("5%","95%"),new=c("lower","upper"))
-    p <- ggplot(plot.data, aes(x=x, y=mean, ymin=lower,ymax=upper)) + geom_line() + geom_ribbon(alpha=0.5) + facet_grid(name.col ~ name.row)
+    setnames(plot.data,old=c("5%","95%"),new=c("lower","upper"),skip_absent=T)
+    p <- ggplot(plot.data, aes(x=x, y=mean, ymin=lower,ymax=upper)) + geom_line() + geom_ribbon(alpha=0.5) + facet_grid(name.row ~ name.col)
     pdf('plots/test_irf.pdf')
     print(p)
     dev.off()
@@ -169,11 +170,45 @@ plot.irf <- function(irf, matnames){
     return(p)
 }
 
+plot.irf.focal <- function(plotdata, matnames, focal.sub = 'seattle',level='95'){
+    plot.data <- copy(plotdata)
+
+    if(level=='95')
+        setnames(plot.data,old=c('97.5%','2.5%'),new=c('upper','lower'),skip_absent=T)
+    if(level=='90')
+        setnames(plot.data,old=c('95%','5%'),new=c('upper','lower'),skip_absent=T)
+    if(level=='80')
+        setnames(plot.data,old=c('85%','15%'),new=c('upper','lower'),skip_absent=T)
+
+    plot.data <- plot.data[(name.row == focal.sub) | (name.col==focal.sub)]
+    plot.data <- plot.data[(name.row != name.col)]
+    plot.data <- plot.data[,":="(name.row = factor(name.row,levels=matnames),
+                                 name.col = factor(name.col,levels=matnames),
+                                 dir=ifelse(name.row==focal.sub,paste0("on ",focal.sub),paste0('of ',focal.sub)),
+                                 name=ifelse(name.row==focal.sub,name.col,name.row)
+                                 )]
+    
+
+
+    p <- ggplot(plot.data, aes(x=x, y=mean, ymin=lower,ymax=upper)) + geom_line() + geom_ribbon(alpha=0.5) + facet_grid(name~dir)
+    pdf('plots/test_irf_focal.pdf',width=6,height=18)
+    print(p)
+    dev.off()
+
+    return(p)
+
+}
 
 ## we say the short-run relationship is the first one that is statistically significant
-get.community.edgelists <- function(irf.plotdata){
-    setnames(irf.plotdata,old=c('95%','5%'),new=c('upper','lower'))
-    setnames(irf.plotdata,old=c('80%','20%'),new=c('upper.1','lower.1'))
+get.community.edgelists <- function(irf.plotdata,level='90'){
+    irf.plotdata <- copy(irf.plotdata)
+
+    if(level=='95')
+        setnames(irf.plotdata,old=c('97.5%','2.5%'),new=c('upper','lower'),skip_absent=T)
+    if(level=='90')
+        setnames(irf.plotdata,old=c('95%','5%'),new=c('upper','lower'),skip_absent=T)
+    if(level=='80')
+        setnames(irf.plotdata,old=c('85%','15%'),new=c('upper','lower'),skip_absent=T)
 
     first.negative <- irf.plotdata[upper < 0,.(x=min(x)),by=c("name.row","name.col")]
     first.negative <- first.negative[,'type':='competitor']
@@ -188,6 +223,7 @@ get.community.edgelists <- function(irf.plotdata){
  
     long.run <- rbind(last.negative,last.positive)
     long.run <- long.run[long.run[,.(x=max(x)),by=.(name.row,name.col)],on=.(name.row,name.col,x)]
+    short.run <- rbind(first.negative,first.positive)
     short.run <- short.run[short.run[,.(x=min(x)),by=.(name.row,name.col)],on=.(name.row,name.col,x)]
     
     return(list('long.run' = long.run, 'short.run' = short.run))
@@ -202,7 +238,7 @@ plot.forecast <- function(){
 }
 
 if (sys.nframe() == 0){
-    model.name <- 'test_stan_stanmod'
+    model.name <- 'var_stan_p1_stanmod'
     draws <- load.draws(model.name)
     draws <- as.data.table(draws)
     params <- extract.params(draws)
@@ -210,22 +246,34 @@ if (sys.nframe() == 0){
     mu <- params[['mu']]
     phi <- params[['phi']]
 
-    for(i in 1:length(phi)){
-        for(k in 1:dim(phi[[i]])[3]){
-            phi[[i]][,,k] <- matrix(as.numeric(phi[[i]][[k]] * matrix(c(0.5,0.5,-0.5,0.5),2)),2)
-        }
-    }
+    included_subreddits <- data.table(read_feather("data/included_timeseries.feather"))
+    included_subreddits <- unique(included_subreddits$subreddit)
+    ## for(i in 1:length(phi)){
+    ##     for(k in 1:dim(phi[[i]])[3]){
+    ##         phi[[i]][,,k] <- matrix(as.numeric(phi[[i]][[k]] * matrix(c(0.5,0.5,-0.5,0.5),2)),2)
+    ##     }
+    ## }
 
     sigma <- params[['sigma']]
 
                                         # the forecast irf doesn't account for simultaneous covariance.
-    irf.forecast <- get.irf.forecast(phi,4*6)
-    irf.ortho <- get.irf.ortho(phi,sigma,4*6)
-    plot.data <- irf.to.plotdata(irf.ortho,c('seattle','seahawks'))
+    irf.forecast <- get.irf.forecast(phi,8)
+    irf.ortho <- get.irf.ortho(phi,sigma,8)
 
-    plot.irf(irf.ortho, c('seattle','seahawks'))
+    plot.data.forecast <- irf.to.plotdata(irf.forecast,included_subreddits)
 
-    plot.data <- irf.to.plotdata(irf.ortho,c('seattle','seahawks'))
-    get.community.edgelists(plot.data)
+    plot.irf.focal(plot.data.forecast, included_subreddits,focal.sub = 'seattle')
+
+    plot.data <- irf.to.plotdata(irf.ortho,included_subreddits)
+
+    plot.irf.focal(plot.data, included_subreddits,focal.sub = 'seattle')
+
+    plot.data <- irf.to.plotdata(irf.ortho,included_subreddits)
+
+    edgelists <-get.community.edgelists(plot.data,level='95')
+
+    ## remove self-edges
+    shortrun <- edgelists$short
+    shortrun <- shortrun[name.row != name.col]
 
 }
